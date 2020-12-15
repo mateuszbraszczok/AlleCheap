e3b7fb67f9b5ff1225522287a5b529760c1a224b
Mateusz Braszczok <mateusz.braszczok@gmail.com>
2020-12-15 13:54:38 +0100

    next comm

--- index.php
+++ index.php
@@ -5,10 +5,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="styles.css" type="text/css"/>  
+    <title>Alle Cheap</title>
 </head>
 <body>
 
     Hello World!!!
+    seuj9ifrihjewsrfuybewsifyhuesufysehyfesiuhy
 
 <?php echo("<br>Hello WORLD2") ?>
 
