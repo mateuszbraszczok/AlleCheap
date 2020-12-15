604da4dbf07c12db71a3c1107501d1ee50f3ac67
Mateusz Braszczok <mateusz.braszczok@gmail.com>
2020-12-15 12:45:28 +0100

    head update

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
+<!DOCTYPE html>
 <html>
-
-<head>  </head>
+<head>
+<meta charset="UTF-8">	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles.css" type="text/css"/>  
+</head>
 <body>
 
-Hello World!!!
+    Hello World!!!
 
 <?php echo("<br>Hello WORLD2") ?>
+
 </body>
-
-
 </html>
