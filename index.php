7f5e196dd3678910e07ce1e1c3de50b5292c30ad
Mateusz Braszczok <mateusz.braszczok@gmail.com>
2020-12-20 13:53:12 +0100

    Add bootstrap

--- index.php
+++ index.php
@@ -1,25 +1,31 @@
 <!DOCTYPE html>
-<html>
+<html lang="pl">
 <head>
-<meta charset="UTF-8">	
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css"/>
     <link rel="stylesheet" href="styles.css" type="text/css"/>  
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
     <title>Alle Cheap</title>
 </head>
 <body>
 
-    Hello World!!!
-    seuj9ifrihjewsrfuybewsifyhuesufysehyfesiuhy
-
-
-
-    tesertvcxsdfgvcxsfgvcxsfgvcxdser
-    Mateusz test
-
-    test 3  wojtetk
+<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown button
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
 
 <?php echo("<br>Hello WORLD2") ?>
+
 
 </body>
 </html>
