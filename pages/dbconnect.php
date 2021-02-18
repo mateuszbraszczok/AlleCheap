7cd5c5a6e8903dc1f68f5a4a6ce32ec7f3cac08b
Mateusz Braszczok <mateusz.braszczok@gmail.com>
2021-02-18 11:51:40 +0100

    awdwd

---
+++ pages/dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+	$servername = "serwer2057795.home.pl";
+	$username = "33989716_allecheap2";
+	//$dbname = "33989716_allecheap";
+	$dbname = "33989716_allecheap2";
+	//$password = "R4GfSZN9";
+	$password = "32EH8gZx";
+	mysqli_report(MYSQLI_REPORT_STRICT);
+	echo"dupa";
+?> 
